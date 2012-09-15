98c63f5e23a6f3bc00d1183a4cb3cdcff01bd92d
William Hogman <william@hogman.me>
2012-09-15 09:57:31 +0200

    added more emacs things

--- emacs/language-settings/clojure-settings.el
+++ emacs/language-settings/clojure-settings.el
@@ -1,3 +1,5 @@
+(require 'clojure-mode)
+
 (defun clojure-slime-maybe-compile-and-load-file ()
   "Call function `slime-compile-and-load-file' if current buffer is connected to a swank server.
   Meant to be used in `after-save-hook'."
@@ -6,5 +8,65 @@
 
 
 (add-hook 'after-save-hook 'clojure-slime-maybe-compile-and-load-file)
+
+
+
+(eval-after-load 'clojure-mode
+  '(font-lock-add-keywords
+    'clojure-mode `(("(\\(fn\\)[\[[:space:]]"
+                     (0 (progn (compose-region (match-beginning 1)
+                                               (match-end 1) "λ")
+                               nil))))))
+
+(eval-after-load 'clojure-mode
+  '(font-lock-add-keywords
+    'clojure-mode `(("\\(#\\)("
+                     (0 (progn (compose-region (match-beginning 1)
+                                               (match-end 1) "ƒ")
+                               nil))))))
+
+(eval-after-load 'clojure-mode
+  '(font-lock-add-keywords
+    'clojure-mode `(("\\(#\\){"
+                     (0 (progn (compose-region (match-beginning 1)
+                                               (match-end 1) "∈")
+                               nil))))))
+
+(eval-after-load 'find-file-in-project
+  '(add-to-list 'ffip-patterns "*.clj"))
+
+
+
+(add-hook 'clojure-mode-hook
+          (lambda ()
+            (enable-paredit-mode)
+            (rainbow-delimiters-mode)
+            (add-to-list 'ac-sources 'ac-source-yasnippet)
+            (setq buffer-save-without-query t)))
+
+;;command to align let statements
+;;To use: M-x align-cljlet
+;;(live-add-pack-lib "align-cljlet")
+;;(require 'align-cljlet)
+
+;;Treat hyphens as a word character when transposing words
+(defvar clojure-mode-with-hyphens-as-word-sep-syntax-table
+  (let ((st (make-syntax-table clojure-mode-syntax-table)))
+    (modify-syntax-entry ?- "w" st)
+    st))
+
+(defun live-transpose-words-with-hyphens (arg)
+  "Treat hyphens as a word character when transposing words"
+  (interactive "*p")
+  (with-syntax-table clojure-mode-with-hyphens-as-word-sep-syntax-table
+    (transpose-words arg)))
+
+(define-key clojure-mode-map (kbd "M-t") 'live-transpose-words-with-hyphens)
+
+(setq auto-mode-alist (append '(("\\.cljs$" . clojure-mode))
+                              auto-mode-alist))
+
+(dolist (x '(scheme emacs-lisp lisp))
+  (add-hook (intern (concat (symbol-name x) "-mode-hook")) 'rainbow-delimiters-mode))
 
 (provide 'clojure-settings)
