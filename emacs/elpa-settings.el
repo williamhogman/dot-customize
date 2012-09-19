a42f82837e4c8c5d9b54c84629aec47f4ec0a14a
William Hogman <me@whn.se>
2012-09-19 21:38:14 +0200

    added compatibility code for loading package.el on emacs23

--- emacs/elpa-settings.el
+++ emacs/elpa-settings.el
@@ -3,5 +3,11 @@
                          ("emacs-elpa" . "http://elpa.gnu.org/packages")
                          ("melpa" . "http://mepla.milkbox.net/packages")
                          ))
-(package-initialize)
+
+(if (fboundp 'package-initialize)
+  (package-initialize)
+  (progn
+    (load (concat (getenv "C_HOME")) "/emacs/compat/package.el")
+    (package-initialize)))
+
 (provide 'elpa-settings)
