82b38bfc24fab01fed1f457bc80b49a8b0f57fec
William Hogman <me@whn.se>
2012-09-22 09:32:21 +0200

    ESS settings now fallback to using site-lisp instead of the other way around

--- emacs/ess-settings.el
+++ emacs/ess-settings.el
@@ -3,4 +3,7 @@
     (progn (add-to-list 'load-path "/usr/share/emacs/site-lisp/ess")
            (require 'ess-site)))
 
+(setq ess-eval-visibly-p nil) ;otherwise C-c C-r (eval region) takes forever
+(setq ess-ask-for-ess-directory nil) ;otherwise you are prompted each time you start an interactive R session
+
 (provide 'ess-settings)
