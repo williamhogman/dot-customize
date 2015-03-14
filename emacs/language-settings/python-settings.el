bafe4f82476c51ecf9b4ac5b19cb9fdd4ea6d3eb
William Hogman <me@whn.se>
2015-03-14 22:24:37 +0100

    remove broken python completion

--- emacs/language-settings/python-settings.el
+++ emacs/language-settings/python-settings.el
@@ -1,8 +1,2 @@
 (require 'python)
-
-
-;;(setq ipython-command "/usr/bin/ipython")
-(setq ipython-completion-command-string "print(';'.join(get_ipython().Completer.complete('%s')[1])) #PYTHON-MODE SILENT\n")
-
-
 (provide 'python-settings)
