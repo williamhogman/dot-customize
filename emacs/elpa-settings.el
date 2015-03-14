4c12376808c938d0ce72a35daa4ce1330c9faf10
William Hogman <me@whn.se>
2015-03-14 22:37:36 +0100

    remove messed up elpa settings

--- emacs/elpa-settings.el
+++
@@ -1,8 +0,0 @@
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ))
-
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t) ; Org-mode's repository
-
-(provide 'elpa-settings)
