5c34ec1952f9d283e6e1d6b322764c6f21fc0925
William Hogman <me@whn.se>
2015-03-14 21:38:27 +0100

    remove outdated irc

--- emacs/erc-settings.el
+++ emacs/erc-settings.el
@@ -1,17 +1,1 @@
-(require 'tls)
-
-
-(setq
-;; Settings for znc
- znc-servers '(("gandalf.hogman.me" 7565 t
-                ((freenode "w_freenode" "whderp"))))
- )
-
-(defun whn-irc ()
-  (interactive)
-  (select-frame (make-frame '((name . "emacs-irc")
-                              (class . "emacs-irc")
- 			      (minibuffer . t))))
-  (znc-erc 'freenode))
-
 (provide 'erc-settings)
