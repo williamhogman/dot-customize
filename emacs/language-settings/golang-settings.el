105f3b5a1eb1e00fdfe8ec027396f16e5e1770e3
William Hogman <me@whn.se>
2015-03-14 22:16:36 +0100

    remove old golang checking comment

--- emacs/language-settings/golang-settings.el
+++ emacs/language-settings/golang-settings.el
@@ -1,15 +1,5 @@
 (eval-after-load "go-mode"
   (add-hook 'before-save-hook 'gofmt-before-save))
 
-;;
-;;(eval-after-load "go-mode"
-;;  '(progn
-;;     (flycheck-declare-checker go-fmt
-;;       "A Go syntax and style checker using the gofmt utility."
-;;       :command '("gofmt" source-inplace)
-;;       :error-patterns '(("^\\(?1:.*\\):\\(?2:[0-9]+\\):\\(?3:[0-9]+\\): \\(?4:.*\\)$" error))
-;;       :modes 'go-mode)
-;;     (add-to-list 'flycheck-checkers 'go-fmt)))
-;;
 
 (provide 'golang-settings)
