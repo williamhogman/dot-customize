966df0af0e6e6450e5c65113f3492d838a560220
whn <me@whn.se>
2020-05-26 15:13:09 +0200

    Add fishconfig

---
+++ config/fish/config.fish
@@ -0,0 +1,8 @@
+set fish_greeting
+set -x GPG_TTY (tty)
+set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+set -x  GITHUB_OAUTH_TOKEN 6ce0c288c353c173aa63959f707b507bc1e5114c
+set -x PATH "$HOME/.krew/bin:$HOME/.cargo/bin:$PATH"
+set pipenv_fish_fancy yes 
+set -x ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX YES
+gpgconf --launch gpg-agent
