4a14747e9be8a32d9cd73d325c7cd7e68b7022ef
whn <me@whn.se>
2020-05-26 13:18:54 +0200

    Remove unused

--- apps/zsh/theme.zsh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-# https://github.com/blinks zsh theme
-
-if [[ $TERM == "emacs" ]]; then
-    PROMPT="➜"
-else
-    PROMPT='%{$fg_bold[red]%}➜%{$reset_color%}'
-    RPROMPT='%b%{$fg_bold[red]%}%c%{$reset_color%}'
-fi
