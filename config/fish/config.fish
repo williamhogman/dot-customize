0098d48f30ac93c9b34ce2bc0327be49c4d24ee1
whn <me@whn.se>
2020-05-31 21:13:27 +0200

    VISUAL as emacsclient

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,6 +5,7 @@ set -x  GITHUB_OAUTH_TOKEN 6ce0c288c353c173aa63959f707b507bc1e5114c
 set -x PATH "$HOME/.krew/bin:$HOME/.cargo/bin:$PATH"
 set pipenv_fish_fancy yes
 set -x ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX YES
+set -x VISUAL "emacsclient -a nano -q"
 gpgconf --launch gpg-agent
 
 # Chromecast options
